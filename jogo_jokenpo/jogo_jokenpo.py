7e88bf767742d961bd786799de3b2b061d704293
Neto Ribeiro <netorribeiro.2007@gmail.com>
2024-04-29 09:42:47 -0300

    add: criei e salvei o jogo Jokenpo, depois de praticamente termina-lo

--- jogo_jokenpo/jogo_jokenpo.py
+++ jogo_jokenpo/jogo_jokenpo.py
@@ -1,1 +1,31 @@
+import os 
+import random
+os.system("cls")
 print('olá! seja bem vindo ao jokenpô.')
+print('1 ---------> Pedra;')
+print('2 ---------> Papel;')
+print('3 ---------> Tesoura;')
+
+
+while True:
+   
+    escolha = int(input("escolha pedra, papel ou tesoura:"))
+    escolhact = random.randint(1,3)
+
+    if escolha == escolhact: 
+        print ('empatamos, vamos de novo. JO KEN PO!')
+
+    if escolha == 1 and escolhact == 2:
+        print ('papel venceu a pedra, vamos de novo. JO KEN PO!')
+    if escolha == 1 and escolhact == 3:
+        print ('pedra venceu tesoura, vamos de novo. JO KEN PO!')
+
+    if escolha == 2 and escolhact == 1:
+        print ('papel venceu a pedra, vamos de novo. JO KEN PO!')
+    if escolha == 2 and escolhact == 3:
+        print ('tesoura venceu papel, vamos de novo. JO KEN PO!')
+
+    if escolha == 2 and escolhact == 1:
+        print ('pedra venceu a tesoura, vamos de novo. JO KEN PO!')
+    if escolha == 2 and escolhact == 2:
+        print ('tesoura venceu papel, vamos de novo. JO KEN PO!')
