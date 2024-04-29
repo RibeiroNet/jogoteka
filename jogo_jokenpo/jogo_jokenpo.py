4a33a18901291d21ea7b22862cd39f015350d71e
Neto Ribeiro <netorribeiro.2007@gmail.com>
2024-04-29 08:36:15 -0300

    add:  adicionando jokenpo

---
+++ jogo_jokenpo/jogo_jokenpo.py
@@ -0,0 +1,1 @@
+print('olá! seja bem vindo ao jokenpô.')
--- main.py
+++ main.py
@@ -6,40 +6,41 @@ from jogo_da_velha.velha import *
 from jogo_calculadora.calculadora import *
 
 
-os.system("cls")
 
 
-print('''
-******************** SEJA BEM VINDO A ****************************** 
-      
-     ██╗ ██████╗  ██████╗  ██████╗ ████████╗███████╗██╗  ██╗ █████╗ 
-     ██║██╔═══██╗██╔════╝ ██╔═══██╗╚══██╔══╝██╔════╝██║ ██╔╝██╔══██╗
-     ██║██║   ██║██║  ███╗██║   ██║   ██║   █████╗  █████╔╝ ███████║
-██   ██║██║   ██║██║   ██║██║   ██║   ██║   ██╔══╝  ██╔═██╗ ██╔══██║
-╚█████╔╝╚██████╔╝╚██████╔╝╚██████╔╝   ██║   ███████╗██║  ██╗██║  ██║
- ╚════╝  ╚═════╝  ╚═════╝  ╚═════╝    ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝
-''')
-
-idade = int(input("qual sua idade?  "))
-if idade >= 18:
-   print('''escolha um jogo para se divertir.  
-    1.Forca e Enforca 
-    2.Jogo de Adivinhação makumbado
-    3.Calculadora Mortal
-    4.Jogo da Velha Maldito
+while True:
+   os.system("cls")
+   print('''
+   ******************** SEJA BEM VINDO A ****************************** 
          
-''')
-if idade < 18:
-    print("você não pode acessar a jogoteka. não volte mais!")
-    exit()
+      ██╗ ██████╗  ██████╗  ██████╗ ████████╗███████╗██╗  ██╗ █████╗ 
+      ██║██╔═══██╗██╔════╝ ██╔═══██╗╚══██╔══╝██╔════╝██║ ██╔╝██╔══██╗
+      ██║██║   ██║██║  ███╗██║   ██║   ██║   █████╗  █████╔╝ ███████║
+   ██   ██║██║   ██║██║   ██║██║   ██║   ██║   ██╔══╝  ██╔═██╗ ██╔══██║
+   ╚█████╔╝╚██████╔╝╚██████╔╝╚██████╔╝   ██║   ███████╗██║  ██╗██║  ██║
+   ╚════╝  ╚═════╝  ╚═════╝  ╚═════╝    ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝
+   ''')
 
-jogo_escolhido = int(input("Em qual jogo você gostaria de se aventurar?  "))
+   idade = int(input("qual sua idade?  "))
+   if idade >= 18:
+      print('''escolha um jogo para se divertir.  
+      1.Forca e Enforca 
+      2.Jogo de Adivinhação makumbado
+      3.Calculadora Mortal
+      4.Jogo da Velha Maldito
+            
+   ''')
+   if idade < 18:
+      print("você não pode acessar a jogoteka. não volte mais!")
+      exit()
 
-if jogo_escolhido == 2:
-   adivinhacao()
-if jogo_escolhido == 1:
-   forca()
-if jogo_escolhido == 4:
-   velhinha()
-if jogo_escolhido == 3:
-   calcular()
+   jogo_escolhido = int(input("Em qual jogo você gostaria de se aventurar?  "))
+
+   if jogo_escolhido == 2:
+      adivinhacao()
+   if jogo_escolhido == 1:
+      forca()
+   if jogo_escolhido == 4:
+      velhinha()
+   if jogo_escolhido == 3:
+      calcular()
